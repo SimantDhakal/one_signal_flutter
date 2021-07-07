fc61e08d3704fee699430a62c2fff8478807e3dd
Simant Dhakal <Simant.dhakal@gmail.com>
2021-07-07 16:39:45 +0545

    one signal implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:device_info/device_info.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:system_alert_window/system_alert_window.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,6 +14,22 @@ void main() {
     OSiOSSettings.autoPrompt: false,
     OSiOSSettings.inAppLaunchUrl: false
   });
+
+  OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+  // will be called whenever a notification is received
+  OneSignal.shared
+      .setNotificationReceivedHandler((OSNotification notification) {
+    // print('Received Notification: ' + notification?.payload?.body ?? '');
+    print('Received Notification: ' + notification.payload.body);
+    _MyHomePageState().showOverlayWindow();
+  });
+  // will be called whenever a notification is opened/button pressed.
+  OneSignal.shared
+      .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+    print('Opened Notification: ' + result.notification?.payload?.body ?? '');
+    _MyHomePageState().showOverlayWindow();
+  });
+
   runApp(MyApp());
 }
 
@@ -20,19 +41,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -42,16 +51,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -60,17 +59,294 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  int _counter = 0;
+  String _platformVersion = 'Unknown';
+  bool _isShowingWindow = false;
+  bool _isUpdatedWindow = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+
+    _initPlatformState();
+    _checkPermissions();
+    SystemAlertWindow.registerOnClickListener(callBack);
+
+    _handleGetDeviceState();
+  }
+
+  void _handleGetDeviceState() async {
+    print("Getting DeviceState");
+    OneSignal.shared.getPermissionSubscriptionState().then((deviceState) {
+      print("DeviceState: ${deviceState?.jsonRepresentation()}");
     });
+  }
+
+  Future<void> initPlatformState() async {
+    Map<String, dynamic> deviceData = <String, dynamic>{};
+
+    try {
+      if (Platform.isAndroid) {
+        deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _deviceData = deviceData;
+    });
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'androidId': build.androidId,
+      'systemFeatures': build.systemFeatures,
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
+
+
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> _initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await SystemAlertWindow.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  Future<void> _checkPermissions() async {
+    await SystemAlertWindow.checkPermissions;
+  }
+
+  void showOverlayWindow() {
+    if (!_isShowingWindow) {
+      SystemWindowHeader header = SystemWindowHeader(
+          title: SystemWindowText(text: "Incoming Call", fontSize: 16, textColor: Colors.black45),
+          padding: SystemWindowPadding.setSymmetricPadding(12, 12),
+          subTitle: SystemWindowText(text: "+9779861169270", fontSize: 16, fontWeight: FontWeight.BOLD, textColor: Colors.black87),
+          decoration: SystemWindowDecoration(startColor: Colors.grey[100]),
+          button: SystemWindowButton(text: SystemWindowText(text: "Rider Invitation", fontSize: 10, textColor: Colors.black45), tag: "personal_btn"),
+          buttonPosition: ButtonPosition.TRAILING);
+      SystemWindowBody body = SystemWindowBody(
+        rows: [
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Some random description on user data.", fontSize: 12, textColor: Colors.black45),
+              ),
+            ],
+            gravity: ContentGravity.CENTER,
+          ),
+          EachRow(columns: [
+            EachColumn(
+                text: SystemWindowText(text: "Here user will received data information such as rating and other..", fontSize: 12, textColor: Colors.black87, fontWeight: FontWeight.BOLD),
+                padding: SystemWindowPadding.setSymmetricPadding(6, 8),
+                decoration: SystemWindowDecoration(startColor: Colors.black12, borderRadius: 25.0),
+                margin: SystemWindowMargin(top: 4)),
+          ], gravity: ContentGravity.CENTER),
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Notes", fontSize: 10, textColor: Colors.black45),
+              ),
+            ],
+            gravity: ContentGravity.LEFT,
+            margin: SystemWindowMargin(top: 8),
+          ),
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Some random notes.", fontSize: 13, textColor: Colors.black54, fontWeight: FontWeight.BOLD),
+              ),
+            ],
+            gravity: ContentGravity.LEFT,
+          ),
+        ],
+        padding: SystemWindowPadding(left: 16, right: 16, bottom: 12, top: 12),
+      );
+      SystemWindowFooter footer = SystemWindowFooter(
+          buttons: [
+            SystemWindowButton(
+              text: SystemWindowText(text: "Reject Request", fontSize: 12, textColor: Color.fromRGBO(250, 139, 97, 1)),
+              tag: "simple_button",
+              padding: SystemWindowPadding(left: 10, right: 10, bottom: 10, top: 10),
+              width: 0,
+              height: SystemWindowButton.WRAP_CONTENT,
+              decoration: SystemWindowDecoration(
+                  startColor: Colors.redAccent, endColor: Colors.red, borderWidth: 0, borderRadius: 30.0),
+            ),
+            SystemWindowButton(
+              text: SystemWindowText(text: "Accept Request", fontSize: 12, textColor: Colors.white),
+              tag: "focus_button",
+              width: 0,
+              padding: SystemWindowPadding(left: 10, right: 10, bottom: 10, top: 10),
+              height: SystemWindowButton.WRAP_CONTENT,
+              decoration: SystemWindowDecoration(
+                  startColor: Color.fromRGBO(250, 139, 97, 1), endColor: Color.fromRGBO(247, 28, 88, 1), borderWidth: 0, borderRadius: 30.0),
+            )
+          ],
+          padding: SystemWindowPadding(left: 16, right: 16, bottom: 12),
+          decoration: SystemWindowDecoration(startColor: Colors.white),
+          buttonsPosition: ButtonPosition.CENTER);
+      SystemAlertWindow.showSystemWindow(
+          height: 230,
+          header: header,
+          body: body,
+          footer: footer,
+          margin: SystemWindowMargin(left: 8, right: 8, top: 200, bottom: 0),
+          gravity: SystemWindowGravity.TOP);
+      setState(() {
+        _isShowingWindow = true;
+      });
+    } else if (!_isUpdatedWindow) {
+      SystemWindowHeader header = SystemWindowHeader(
+          title: SystemWindowText(text: "Outgoing Call", fontSize: 10, textColor: Colors.black45),
+          padding: SystemWindowPadding.setSymmetricPadding(12, 12),
+          subTitle: SystemWindowText(text: "8989898989", fontSize: 14, fontWeight: FontWeight.BOLD, textColor: Colors.black87),
+          decoration: SystemWindowDecoration(startColor: Colors.grey[100]),
+          button: SystemWindowButton(text: SystemWindowText(text: "Personal", fontSize: 10, textColor: Colors.black45), tag: "personal_btn"),
+          buttonPosition: ButtonPosition.TRAILING);
+      SystemWindowBody body = SystemWindowBody(
+        rows: [
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Updated body", fontSize: 12, textColor: Colors.black45),
+              ),
+            ],
+            gravity: ContentGravity.CENTER,
+          ),
+          EachRow(columns: [
+            EachColumn(
+                text: SystemWindowText(text: "Updated long data of the body", fontSize: 12, textColor: Colors.black87, fontWeight: FontWeight.BOLD),
+                padding: SystemWindowPadding.setSymmetricPadding(6, 8),
+                decoration: SystemWindowDecoration(startColor: Colors.black12, borderRadius: 25.0),
+                margin: SystemWindowMargin(top: 4)),
+          ], gravity: ContentGravity.CENTER),
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Notes", fontSize: 10, textColor: Colors.black45),
+              ),
+            ],
+            gravity: ContentGravity.LEFT,
+            margin: SystemWindowMargin(top: 8),
+          ),
+          EachRow(
+            columns: [
+              EachColumn(
+                text: SystemWindowText(text: "Updated random notes.", fontSize: 13, textColor: Colors.black54, fontWeight: FontWeight.BOLD),
+              ),
+            ],
+            gravity: ContentGravity.LEFT,
+          ),
+        ],
+        padding: SystemWindowPadding(left: 16, right: 16, bottom: 12, top: 12),
+      );
+      SystemWindowFooter footer = SystemWindowFooter(
+          buttons: [
+            SystemWindowButton(
+              text: SystemWindowText(text: "Updated Simple button", fontSize: 12, textColor: Color.fromRGBO(250, 139, 97, 1)),
+              tag: "updated_simple_button",
+              padding: SystemWindowPadding(left: 10, right: 10, bottom: 10, top: 10),
+              width: 0,
+              height: SystemWindowButton.WRAP_CONTENT,
+              decoration: SystemWindowDecoration(startColor: Colors.white, endColor: Colors.white, borderWidth: 0, borderRadius: 0.0),
+            ),
+            SystemWindowButton(
+              text: SystemWindowText(text: "Focus button", fontSize: 12, textColor: Colors.white),
+              tag: "focus_button",
+              width: 0,
+              padding: SystemWindowPadding(left: 10, right: 10, bottom: 10, top: 10),
+              height: SystemWindowButton.WRAP_CONTENT,
+              decoration: SystemWindowDecoration(
+                  startColor: Color.fromRGBO(250, 139, 97, 1), endColor: Color.fromRGBO(247, 28, 88, 1), borderWidth: 0, borderRadius: 30.0),
+            )
+          ],
+          padding: SystemWindowPadding(left: 16, right: 16, bottom: 12),
+          decoration: SystemWindowDecoration(startColor: Colors.white),
+          buttonsPosition: ButtonPosition.CENTER);
+      SystemAlertWindow.updateSystemWindow(
+          height: 230,
+          header: header,
+          body: body,
+          footer: footer,
+          margin: SystemWindowMargin(left: 8, right: 8, top: 200, bottom: 0),
+          gravity: SystemWindowGravity.TOP);
+      setState(() {
+        _isUpdatedWindow = true;
+      });
+    } else {
+      setState(() {
+        _isShowingWindow = false;
+        _isUpdatedWindow = false;
+      });
+      SystemAlertWindow.closeSystemWindow();
+    }
   }
 
   @override
@@ -79,27 +355,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: _deviceData.keys.map((String property) {
+          return Row(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(property),
+              ),
+              Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                    child: Text(
+                      '${_deviceData[property]}',
+                      maxLines: 10,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )),
+            ],
+          );
+        }).toList(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+void callBack(String tag) {
+  print(tag);
+  switch (tag) {
+    case "simple_button":
+    case "updated_simple_button":
+      SystemAlertWindow.closeSystemWindow();
+      break;
+    case "focus_button":
+      print("Focus button has been called");
+      break;
+    default:
+      print("OnClick event of $tag");
   }
 }
